534f50ec0c6e068996202436f6465f58585451e5
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2024-03-31 14:57:05 +0300

    Adding types

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
+	var investmentAmount float64 = 1000
+	var expectedReturnRate float64 = 5.5
 	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), years)
+	var futureValue = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
 
 	fmt.Println(futureValue)
 
