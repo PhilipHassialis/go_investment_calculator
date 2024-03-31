1bac99b764f07ca32d72f4141135f6641ebdb620
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2024-03-31 19:35:17 +0300

    Multiple return values function approach

--- investment_calculator.go
+++ investment_calculator.go
@@ -16,18 +16,15 @@ func main() {
 	fmt.Print("Expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
-	futureRealValue := calculateFutureRealValue(futureValue, inflationRate, years)
+	futureValue, futureRealValue := calculateValues(investmentAmount, expectedReturnRate, inflationRate, years)
 
 	formattedFV := fmt.Sprintf("Future value: %.2f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future value (adj. for inflation): %.2f\n", futureRealValue)
 	fmt.Print(formattedFV, formattedRFV)
 }
 
-func calculateFutureValue(investmentAmount, expectedReturnRate, years float64) float64 {
-	return investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-}
-
-func calculateFutureRealValue(futureValue, inflationRate, years float64) float64 {
-	return futureValue / math.Pow((1+inflationRate/100), years)
+func calculateValues(investmentAmount, expectedReturnRate, inflationRate, years float64) (float64, float64) {
+	fv := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	frv := fv / math.Pow((1+inflationRate/100), years)
+	return fv, frv
 }
