062ec8e44d719c27603a94d9f5e31996f6a15e32
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2024-03-31 14:55:02 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PhilipHassialis/go_investment_calculator
+
+go 1.21.6
---
+++ investment_calculator.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years float64 = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), years)
+
+	fmt.Println(futureValue)
+
+}
