086cbf95be17e5271095dd213a180a78e331f226
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2024-03-31 19:39:01 +0300

    Named multiple return values and simple return approach

--- investment_calculator.go
+++ investment_calculator.go
@@ -23,8 +23,8 @@ func main() {
 	fmt.Print(formattedFV, formattedRFV)
 }
 
-func calculateValues(investmentAmount, expectedReturnRate, inflationRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	frv := fv / math.Pow((1+inflationRate/100), years)
-	return fv, frv
+func calculateValues(investmentAmount, expectedReturnRate, inflationRate, years float64) (fv float64, frv float64) {
+	fv = investmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	frv = fv / math.Pow((1+inflationRate/100), years)
+	return
 }
